ecb242f15e314b2be6a1bd315ad5d500cc36f8c2
aliyast <1793192224@qq.com>
2025-06-10 16:39:52 +0800

    feat:rag api 实现

---
+++ ai-rag-api/src/main/java/cn/ali/ai/rag/api/IRAGService.java
@@ -0,0 +1,14 @@
+package cn.ali.ai.rag.api;
+
+import cn.ali.ai.rag.api.response.Response;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IRAGService {
+
+    Response<List<String>> queryRagTagList();
+
+    Response<String> uploadFile(String ragTag, List<MultipartFile> files);
+
+}
